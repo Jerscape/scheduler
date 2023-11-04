dfbd06609326b2482c02a8f40e31ae98567b68aa
Jeremy  Dutton <jeremyduttonmacbook@Jeremys-MBP.hitronhub.home>
2023-11-04 10:37:00 -0500

    COMMITTING BEFORE CONTINUTING WITH CYPRESS

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -9,3 +9,12 @@ afterEach(cleanup);
 it("renders without crashing", () => {
   render(<Application />);
 });
+
+
+it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+  const { container } = render(<Application />);
+
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  console.log(prettyDOM(container));
+});
